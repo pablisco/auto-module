7d9aa31e494eb4204bc580a93d726402ca685bdb
Pablisco <pabs87@gmail.com>
2020-09-03 12:08:31 +0100

    Simplified AutoModule extension dsl

--- plugin/src/main/kotlin/_AutoModule.kt
+++ plugin/src/main/kotlin/_AutoModule.kt
@@ -1,11 +1,9 @@
 import com.pablisco.gradle.automodule.AutoModule
 import org.gradle.api.initialization.Settings
-import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.configure
 
 /**
  * Extension to be used from scripts. Using default package to avoid imports.
  */
 @Suppress("unused") // Api
-fun Settings.autoModule(block: AutoModule.() -> Unit) {
-    extensions.getByType<AutoModule>().block()
-}
+fun Settings.autoModule(block: AutoModule.() -> Unit): Unit = configure(block)
